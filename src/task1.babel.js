42f9f9e36db541d0b35e4d7b71f58241a9908791
Vladislav Kudashev <Vladislav_Kudashev@epam.com>
2020-08-10 19:04:37 +0300

    chore: removed vanila versions of tasks

--- src/task1.babel.js
+++
@@ -1,7 +0,0 @@
-import { EOL } from "os";
-
-const reverseInput = (input) => {
-  process.stdout.write(`${input.reverse()}${EOL}`);
-};
-
-process.stdin.on("data", reverseInput);
--- src/task2.js
+++ src/task2.js
@@ -1,14 +1,40 @@
-const { csv } = require("csvtojson");
-const fs = require("fs");
+import { csv } from "csvtojson";
+import path from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline, Transform } from "stream";
+import { EOL } from "os";
 
-const csvReader$ = fs
-  .createReadStream("./csv/nodejs-hw1-ex1.csv")
-  .on("error", console.log);
-const jsonWriter$ = fs
-  .createWriteStream("./json/nodejs-hw1-ex1.txt")
-  .on("error", console.log);
+const jsonTransformer = new Transform({
+  transform(data, encoding, callback) {
+    try {
+      const parsedRow = JSON.parse(data);
+      const formattedRow = Object.entries(parsedRow).reduce(
+        (acc, [key, value]) =>
+          Object.assign({}, acc, {
+            [key.toLowerCase()]: value,
+          }),
+        {}
+      );
+      const formattedBuffer = Buffer.from(
+        `${JSON.stringify(formattedRow)}${EOL}`
+      );
+      callback(null, formattedBuffer);
+    } catch (e) {
+      callback(e);
+    }
+  },
+});
+
+const csvReader$ = createReadStream(
+  path.resolve(__dirname, "../csv/nodejs-hw1-ex1.csv")
+).on("error", console.log);
+
+const jsonWriter$ = createWriteStream(
+  path.resolve(__dirname, "../json/nodejs-hw1-ex1.txt")
+).on("error", console.log);
+
 const csvConverter$ = csv({
   ignoreColumns: /amount/i,
 }).on("error", console.log);
 
-csvReader$.pipe(csvConverter$).pipe(jsonWriter$);
+pipeline(csvReader$, csvConverter$, jsonTransformer, jsonWriter$);
